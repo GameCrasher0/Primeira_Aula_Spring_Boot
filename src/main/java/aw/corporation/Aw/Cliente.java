31d8c9b03bb9cd1207234dfdd76cd63c8ee87859
GameCrasher <andrelwolski@hotmail.com>
2022-10-11 21:54:54 -0300

    commit final do dia

--- src/main/java/aw/corporation/Aw/Cliente.java
+++ src/main/java/aw/corporation/Aw/Cliente.java
@@ -1,10 +1,13 @@
 package aw.corporation.Aw;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Cliente {
 
+    @Id
+    private Long id;
     private String cpf;
     private String nome;
 
